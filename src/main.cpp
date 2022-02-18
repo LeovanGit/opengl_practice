f7f598b763e2a4b6636266511a991824e2781da8
LeovanGit <vova11072003@gmail.com>
2022-02-18 21:21:56 +0200

    Materials demo

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,60 @@ Camera camera(glm::vec3(0.0f, 0.0f, 3.0f),
               5.0f,
               0.1);
 
-glm::vec3 light_position(1.8f, 0.5f, 3.0f);
+//glm::vec3 light_position(1.8f, 0.5f, 3.0f);
+glm::vec3 light_position(0.0f, 3.0f, 5.0f);
+
+
+class Material
+{
+public:
+    Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess) :
+        ambient(ambient), diffuse(diffuse), specular(specular), shininess(shininess) {}
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    float shininess;
+};
+
+Material gold(glm::vec3(0.24725, 0.1995, 0.0745),
+              glm::vec3(0.75164, 0.60648, 0.22648),
+              glm::vec3(0.628281, 0.555802, 0.366065),
+              4);
+
+Material obsidian(glm::vec3(0.05375, 0.05, 0.06625),
+                  glm::vec3(0.18275, 0.17, 0.22525),
+                  glm::vec3(0.332741, 0.328634, 0.346435),
+                  3);
+
+Material emerald(glm::vec3(0.0215, 0.1745, 0.0215),
+                 glm::vec3(0.07568, 0.61424, 0.07568),
+                 glm::vec3(0.633, 0.7277811, 0.633),
+                 6);
+
+Material pearl(glm::vec3(0.25, 0.20725, 0.20725),
+               glm::vec3(1.0, 0.829, 0.829),
+               glm::vec3(0.296648),
+               0.88);
+
+Material ruby(glm::vec3(0.1745, 0.01175, 0.01175),
+              glm::vec3(0.61424, 0.04136, 0.04136),
+              glm::vec3(0.727811, 0.626959, 0.626959),
+              6);
+
+Material brass(glm::vec3(0.329412, 0.223529, 0.027451),
+               glm::vec3(0.780392, 0.569627, 0.113725),
+               glm::vec3(0.992157, 0.941176, 0.807843),
+               2.1794872);
+
+Material bronze(glm::vec3(0.2125, 0.1275, 0.054),
+                glm::vec3(0.714, 0.4284, 0.18144),
+                glm::vec3(0.393548, 0.271906, 0.166721),
+                2);
+
+Material chrome(glm::vec3(0.25),
+                glm::vec3(0.4),
+                glm::vec3(0.774597),
+                6);
 
 void calc_delta_time()
 {
@@ -160,6 +213,27 @@ int main()
     glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
+    glm::vec3 cubes[] = {
+        glm::vec3( 0.0f, 0.0f, 0.0f),
+        glm::vec3( 1.5f, 0.0f, 0.0f),
+        glm::vec3( 3.0f, 0.0f, 0.0f),
+        glm::vec3(-1.5f, 0.0f, 0.0f),
+
+        glm::vec3( 0.0f, 1.5f, 0.0f),
+        glm::vec3( 1.5f, 1.5f, 0.0f),
+        glm::vec3( 3.0f, 1.5f, 0.0f),
+        glm::vec3(-1.5f, 1.5f, 0.0f)};
+
+    Material cube_materials[] = {
+        gold,
+        obsidian,
+        emerald,
+        pearl,
+        ruby,
+        brass,
+        bronze,
+        chrome};
+
     while (!glfwWindowShouldClose(window))
     {
         calc_delta_time();
@@ -196,29 +270,33 @@ int main()
         // draw objects
         shader.use();
 
-        model_matrix = glm::mat4(1.0f);
-        model_matrix = glm::rotate(model_matrix, float(glfwGetTime()), glm::vec3(0.0f, 1.0f, 0.0f));
-        model_matrix = glm::scale(model_matrix, glm::vec3(glm::min(float(glfwGetTime()), 1.0f)));
+        for (int i = 0; i != 8; ++i)
+        {
+            model_matrix = glm::mat4(1.0f);
+            model_matrix = glm::translate(model_matrix, cubes[i]);
+          // model_matrix = glm::rotate(model_matrix, float(glfwGetTime()), glm::vec3(0.0f, 1.0f, 0.0f));
+          // model_matrix = glm::scale(model_matrix, glm::vec3(glm::min(float(glfwGetTime()), 1.0f)));
 
-        shader.set_uniform_mat4("model_matrix", model_matrix);
-        shader.set_uniform_mat4("view_matrix", camera.get_view_matrix());
-        shader.set_uniform_mat4("proj_matrix", proj_matrix);
+            shader.set_uniform_mat4("model_matrix", model_matrix);
+            shader.set_uniform_mat4("view_matrix", camera.get_view_matrix());
+            shader.set_uniform_mat4("proj_matrix", proj_matrix);
 
-        shader.set_uniform_vec3("camera_position", camera.get_position());
+            shader.set_uniform_vec3("camera_position", camera.get_position());
 
-        shader.set_uniform_vec3("material.ambient", glm::vec3(0.08f, 0.22f, 0.54f));
-        shader.set_uniform_vec3("material.diffuse", glm::vec3(0.08f, 0.22f, 0.54f));
-        shader.set_uniform_vec3("material.specular", glm::vec3(0.5f));
-        shader.set_uniform_float("material.shininess", 32.0f);
+            shader.set_uniform_vec3("material.ambient", cube_materials[i].ambient);
+            shader.set_uniform_vec3("material.diffuse", cube_materials[i].diffuse);
+            shader.set_uniform_vec3("material.specular", cube_materials[i].specular);
+            shader.set_uniform_float("material.shininess", cube_materials[i].shininess * 128.0f);
 
-        shader.set_uniform_vec3("light.position", light_position);
-        shader.set_uniform_vec3("light.ambient", glm::vec3(0.2f));
-        shader.set_uniform_vec3("light.diffuse", glm::vec3(0.4f));
-        shader.set_uniform_vec3("light.specular", glm::vec3(0.5f));
+            shader.set_uniform_vec3("light.position", light_position);
+            shader.set_uniform_vec3("light.ambient", glm::vec3(1.0f));
+            shader.set_uniform_vec3("light.diffuse", glm::vec3(1.0f));
+            shader.set_uniform_vec3("light.specular", glm::vec3(1.0f));
 
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glBindVertexArray(0);
+        }
 
         glfwSwapBuffers(window);
     }
--- src/shaders/fragment.frag
+++ src/shaders/fragment.frag
@@ -2,29 +2,19 @@
 
 struct Material
 {
-    // this is a colors of each lighting component
-    vec3 ambient;  // same as object color
-    vec3 diffuse;  // color under diffuse light
-    vec3 specular; // color of reflected light
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
     float shininess;
-    // P.S. т.е цвет поверхности будет ambient, но на сторону на которую падает свет,
-    // итоговый цвет будет состоять из ambient + diffuse. В этом легко убедиться
-    // сделав ambient = красный, diffuse = синий, освещенная сторона будет феолетовой, а
-    // теневая сторона останется красной
 };
 
 struct Light
 {
     vec3 position;
 
-    // something like old strength values
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
-    // P.S. это грубо говоря какими цветами стреляет источник света для каждого из компонентов освещения
-    // стоит помнить: если к примеру источник света стреляет красным и синим лучом (т.е. феолетовым),
-    // но материал поверхности отражает только синий (а красный поглащает), то цвет объекта будет синим.
-    // собственно это и зашито в формулу: (1.0, 0.0, 1.0) * (0.0, 0.0, 1.0) = (0.0, 0.0, 1.0)
 };
 
 uniform Material material;
@@ -47,10 +37,9 @@ void main()
     vec3 diffuse = cosa * material.diffuse * light.diffuse;
 
     // specular
-    float shininess = 32;
     vec3 view_ray = normalize(camera_position - vert_frag_position);
     vec3 reflected_ray = reflect(-ray, vert_v_normal);
-    float cosb = pow(max(dot(reflected_ray, view_ray), 0.0f), shininess);
+    float cosb = pow(max(dot(reflected_ray, view_ray), 0.0f), material.shininess);
     vec3 specular = material.specular * cosb * light.specular;
 
     frag_color = vec4(ambient + diffuse + specular, 1.0);
